c1e74a1dd10bfd5755b6b520badfa55425b1809a
kalilzsantos <kalilzsantos@gmail.com>
2020-01-17 19:38:15 -0300

    Incrementando o I18n e deixando traduções dinâmicas

--- Ruby/time_to_answer/app/helpers/admins_backoffice_helper.rb
+++ Ruby/time_to_answer/app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,10 @@
 module AdminsBackofficeHelper
+
+    def translate_attribute(object = nil, method = nil)
+        if object && object
+            object.model.human_attribute_name(method)
+        else
+            "Informe os parâmetros corretos!"
+        end
+    end
 end
